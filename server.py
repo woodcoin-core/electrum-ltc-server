ad1b9450d543a104eec79a753a30b4c9f77306ed
pooler <pooler@litecoinpool.org>
2014-04-01 16:43:56 +0200

    Litecoin: genesis block hash and address version

--- server.py
+++ server.py
@@ -60,7 +60,7 @@ def create_config():
     config.set('server', 'password', '')
     config.set('server', 'irc', 'no')
     config.set('server', 'irc_nick', '')
-    config.set('server', 'coin', '')
+    config.set('server', 'coin', 'litecoin')
     config.set('server', 'datadir', '')
 
     # use leveldb as default
--- utils/__init__.py
+++ utils/__init__.py
@@ -107,7 +107,7 @@ def public_key_to_bc_address(public_key):
     return hash_160_to_bc_address(hash_160(public_key))
 
 
-def hash_160_to_bc_address(h160, addrtype = 0):
+def hash_160_to_bc_address(h160, addrtype = 48):
     if h160 == 'None':
         return 'None'
     vh160 = chr(addrtype) + h160
