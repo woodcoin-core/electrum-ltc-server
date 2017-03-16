2ebc0be19b60197cba36bb2af3bb8c106a0ba429
Scott King <Lvl4Sword@users.noreply.github.com>
2017-03-16 14:23:38 -0600

    Update setup.py
    LTC -> Woodcoin

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup
 
 setup(
-    name="electrum-ltc-server",
+    name="electrum-wdc-server",
     version="1.0",
-    scripts=['run_electrum_ltc_server.py','electrum-ltc-server'],
+    scripts=['run_electrum_wdc_server.py','electrum-wdc-server'],
     install_requires=['plyvel','jsonrpclib', 'irc >= 11, <=14.0'],
     package_dir={
-        'electrumltcserver':'src'
+        'electrumwdcserver':'src'
         },
     py_modules=[
-        'electrumltcserver.__init__',
-        'electrumltcserver.utils',
-        'electrumltcserver.storage',
-        'electrumltcserver.deserialize',
-        'electrumltcserver.networks',
-        'electrumltcserver.blockchain_processor',
-        'electrumltcserver.server_processor',
-        'electrumltcserver.processor',
-        'electrumltcserver.version',
-        'electrumltcserver.ircthread',
-        'electrumltcserver.stratum_tcp'
+        'electrumwdcserver.__init__',
+        'electrumwdcserver.utils',
+        'electrumwdcserver.storage',
+        'electrumwdcserver.deserialize',
+        'electrumwdcserver.networks',
+        'electrumwdcserver.blockchain_processor',
+        'electrumwdcserver.server_processor',
+        'electrumwdcserver.processor',
+        'electrumwdcserver.version',
+        'electrumwdcserver.ircthread',
+        'electrumwdcserver.stratum_tcp'
     ],
     description="Litecoin Electrum Server",
     author="Thomas Voegtlin",
     author_email="thomasv@electrum.org",
     license="MIT Licence",
-    url="https://github.com/pooler/electrum-ltc-server/",
-    long_description="""Server for the Electrum Lightweight Litecoin Wallet"""
+    url="https://github.com/woodcoin-core/electrum-wdc-server/",
+    long_description="""Server for the Electrum Lightweight Woodcoin Wallet"""
 )
